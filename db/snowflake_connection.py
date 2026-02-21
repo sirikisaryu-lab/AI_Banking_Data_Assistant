2c6819d0e8152f07c92c257f055ba870f4359443
sirikisaryu-lab <sirikisaryu@gmail.com>
2026-02-21 16:11:11 +0530

    Add files via upload

---
+++ db/snowflake_connection.py
@@ -0,0 +1,13 @@
+import snowflake.connector
+from config.settings import settings
+
+def get_connection():
+    return snowflake.connector.connect(
+        user=settings.SNOWFLAKE_USER,
+        password=settings.SNOWFLAKE_PASSWORD,
+        account=settings.SNOWFLAKE_ACCOUNT,
+        warehouse=settings.SNOWFLAKE_WAREHOUSE,
+        database=settings.SNOWFLAKE_DATABASE,
+        schema=settings.SNOWFLAKE_SCHEMA,
+        role=settings.SNOWFLAKE_ROLE
+    )
